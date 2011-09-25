3c634767ffc1d64b6f5d695fd46eb12cbe25b6ed
Mark Harris <harrism@gmail.com>
2011-09-25 13:11:34 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForageMaps::Application.config.secret_token = '6a10c1755a6899d4d07e81343e842244c0be114b8c84ec438c81abcccf888eb5df46f6a544c57374371e1aabfc359dd7d04065d286de6e07028503669a8c91ef'
