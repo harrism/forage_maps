44a5aa58a68712edfa95392e84817bc8cb9b184a
Mark Harris <harrism@gmail.com>
2011-09-26 12:51:51 +1000

    Added logo helper and test for links from the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  # Return a logo image tag
+  def logo
+    @logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
   # Return a title on a per-page basis.
   def title
     base_title = "ForageMaps"
