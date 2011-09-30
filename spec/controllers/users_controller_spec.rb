a4c61404e536bb72b0d4a9a4d1ce7c557e102119
Mark Harris <harrism@gmail.com>
2011-09-30 22:54:30 +1000

    Finished sign in/out

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -107,6 +107,11 @@ describe UsersController do
         end.should change(User, :count).by(1)
       end
       
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
+      
       it "should redirect to the user show page" do
         post :create, :user => @attr
         response.should redirect_to(user_path(assigns(:user)))
@@ -116,6 +121,7 @@ describe UsersController do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to ForageMaps/i
       end
+      
     end
   end
 end
